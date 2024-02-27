f6c43ab028315b7ba67a5b6be9512941126f8eff
Adarsh Kumar Mishra <124551477+Adarshabinash@users.noreply.github.com>
2024-02-27 16:14:08 +0530

    This is the View component that wraps the whole thing. Basically like a div.

--- pages/FirstPage.js
+++ pages/FirstPage.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import {Text, View, Image} from 'react-native';
-// const cokeImage = require('../assets/coke1.jpg'); //? Can use require property if we want....
-import cocacola from '../assets/coke1.jpg';
+import {View} from 'react-native';
 
 const FirstPage = () => {
   return (
-    <View style={{backgroundColor: 'plum', flex: 1, alignItems: 'center'}}>
-      <Text>
-        <Text style={{fontWeight: 'bold', color: 'white'}}>Hello</Text> World
-      </Text>
-
-      <Image source={cocacola} />
-    </View>
+    <View
+      style={{backgroundColor: 'plum', flex: 1, alignItems: 'center'}}></View>
   );
 };
 
