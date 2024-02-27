1c752d8e9126c07b617e53951b58fcc861d590c7
Adarsh Kumar Mishra <124551477+Adarshabinash@users.noreply.github.com>
2024-02-27 17:00:52 +0530

    The Image component takes the source which has the image value

--- pages/FirstPage.js
+++ pages/FirstPage.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
+import cocacola from '../assets/coke1.jpg';
+const cocacola2 = require('../assets/coke2.jpg'); //?------We can use the require function to hold our image
 
 const FirstPage = () => {
   return (
-    <View style={{backgroundColor: 'plum', flex: 1, alignItems: 'center'}}>
+    <View style={{backgroundColor: 'plum', flex: 1}}>
       <Text>This is a text</Text>
-      <Text>
-        <Text style={{color: 'white'}}>This is {''}</Text>also a text
-      </Text>
+      <Image source={cocacola} style={{height: '30%', width: '60%'}} />
+      <Image source={cocacola2} style={{height: '30%', width: '60%'}} />
+      <Image
+        source={{
+          uri: 'https://fastly.picsum.photos/id/855/200/200.jpg?hmac=l4U_O6zoVhjz9BqOito1u4k30FNJz3hLVYjvrdE59MU',
+        }}
+        style={{height: 200, width: 200}}
+      />
     </View>
   );
 };
