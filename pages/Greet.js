d3944bb6837bdb5051de8910c1f96b22860bdeeb
Adarsh Kumar Mishra <124551477+Adarshabinash@users.noreply.github.com>
2024-03-01 11:01:44 +0530

    The elevation property is used for the android shadow effect whereas we can define the specified shadows for the ios

--- pages/Greet.js
+++ pages/Greet.js
@@ -4,10 +4,10 @@ import {View, Button, Text, StyleSheet} from 'react-native';
 export default Greet = () => {
   return (
     <View style={styles.container}>
-      <View style={[styles.lightblueBox, styles.box]}>
+      <View style={[styles.lightblueBox, styles.box, styles.boxShadow]}>
         <Text>Light-blue box</Text>
       </View>
-      <View style={[styles.lightgreenBox, styles.box]}>
+      <View style={[styles.lightgreenBox, styles.box, styles.androidShadow]}>
         <Text>Light-green box</Text>
       </View>
     </View>
@@ -21,11 +21,11 @@ export const styles = StyleSheet.create({
     padding: '20%',
   },
   box: {
-    width: '50%',
+    width: '60%',
     height: '30%',
     paddingHorizontal: '10%',
     marginVertical: '3%',
-    borderWidth: 3,
+    borderWidth: 2,
     borderColor: 'black',
     borderRadius: 5,
   },
@@ -35,5 +35,21 @@ export const styles = StyleSheet.create({
   },
   lightgreenBox: {
     backgroundColor: 'lightgreen',
+  },
+  boxShadow: {
+    //^---------------------In case of the box shadow, we have to mention all the properties separatly...
+    shadowColor: 'grey', //?-----------shadowColor- For the color of the assigned shadow.
+    shadowOffset: {
+      //?-------------shadowOffset defines the width and height of the shadow that will be assigned
+      width: 6,
+      height: 3,
+    },
+    shadowOpacity: 0.6, //For the transparency of the shadow
+    shadowRadius: 4,
+  },
+  //^--------In the android device, we have to use the elevation property so that we can add the shadow effect. However the above boxShadow is good for the iOS devices
+  androidShadow: {
+    elevation: 10,
+    shadowColor: 'red',
   },
 });
