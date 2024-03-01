05e248ae32f4bfe8dbed23eaef88a9238cc73063
Adarsh Kumar Mishra <124551477+Adarshabinash@users.noreply.github.com>
2024-03-01 15:43:27 +0530

    The flex property helps in taking in the total available spaces by a conponent

--- pages/Layouts/Layout.js
+++ pages/Layouts/Layout.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
-import Box from '../../components/Box';
+import Box from '../../components/box';
 
 export default function Layout() {
   return (
     <>
       <View style={styles.container}>
         <Box style={{backgroundColor: 'lightgrey'}}>Box1</Box>
-        <Box style={{backgroundColor: 'yellow'}}>Box1</Box>
+        <Box style={{backgroundColor: 'yellow', flex: 3}}>Box1</Box>
         <Box style={{backgroundColor: 'plum'}}>Box2</Box>
         <Box style={{backgroundColor: 'coral'}}>Box3</Box>
         <Box style={{backgroundColor: 'lightblue'}}>Box4</Box>
@@ -23,5 +23,6 @@ const styles = StyleSheet.create({
     marginTop: 30,
     borderWidth: 3,
     borderColor: 'black',
+    flex: 1,
   },
 });
