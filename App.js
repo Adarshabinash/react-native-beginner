21ea2afc6eb9214b3e8e993dfd6be475190f0da6
Adarsh Kumar Mishra <124551477+Adarshabinash@users.noreply.github.com>
2024-03-01 11:58:05 +0530

    Faced some issues with the Layout, soon deal with it

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 // import Greet from './pages/Stylings/Greet';
-import Layout from './pages/Layouts/Layout';
 import {View} from 'react-native';
 
 export default App = () => {
   return (
     <View>
-      <Layout />
+
     </View>
   );
 };
