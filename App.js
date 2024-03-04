af24ee0e1b96e444fad13a9d74b8f20331d17d63
Adarsh Kumar Mishra <124551477+Adarshabinash@users.noreply.github.com>
2024-03-04 17:57:10 +0530

    Made changes to the Pokemon Lists to map through all the pokemons

--- App.js
+++ App.js
@@ -8,12 +8,71 @@ import {View, StyleSheet, Platform} from 'react-native';
 import PokemonCard from './components/Pokemons/PokemonCard';
 
 const App = () => {
+  const pokemonData = [
+    {
+      name: 'Charmander',
+      image: require('./assets/Pokemons/Charmander.jpg'),
+      type: 'Fire 🔥',
+      hp: 40,
+      moves: ['Scratch', 'Ember', 'Growl', 'Leer'],
+      weakness: ['Water', 'Rock'],
+    },
+    {
+      name: 'Pikachu',
+      image: require('./assets/Pokemons/Pikachu.jpg'),
+      type: 'Electric ⚡',
+      hp: 46,
+      moves: ['Thunderbolt', 'Scratch', 'Iron Tail', 'Quick'],
+      weakness: ['Water', 'Rock'],
+    },
+    {
+      name: 'Squirtle',
+      image: require('./assets/Pokemons/Squirtle.jpg'),
+      type: 'Water 💧',
+      hp: 40,
+      moves: ['Water Gun', 'Ember', 'Shell Spin', 'Scratch'],
+      weakness: ['Electric', 'Grass'],
+    },
+    {
+      name: 'Bulbasaur',
+      image: require('./assets/Pokemons/Bulbasaur.jpg'),
+      type: 'Grass 🍃',
+      hp: 42,
+      moves: ['Leaf', 'Whip', 'Scratch', 'Tackle'],
+      weakness: ['Psychic', 'Rock'],
+    },
+    {
+      name: 'Marowak',
+      image: require('./assets/Pokemons/Marowak.jpg'),
+      type: 'Bone 🦴',
+      hp: 36,
+      moves: ['Scratch', 'Bone', 'Bonemerang', 'Scratch'],
+      weakness: ['Water', 'Electric'],
+    },
+    {
+      name: 'Dragonite',
+      image: require('./assets/Pokemons/Dragonite.png'),
+      type: 'Dragon 🔥',
+      hp: 66,
+      moves: ['Fly', 'Punch', 'Sonic', 'Hyperbeam'],
+      weakness: ['Rock', 'Electric'],
+    },
+    {
+      name: 'Pidgeot',
+      image: require('./assets/Pokemons/Pidgeot.png'),
+      type: 'Fly 🦅',
+      hp: 46,
+      moves: ['Fly', 'Gust Wind', 'Wing', 'Scratch'],
+      weakness: ['Fire', 'Electric'],
+    },
+  ];
+
   return (
     <View style={styles.container}>
       {/* <Layout /> */}
       {/* <FlexBasicProps /> */}
       {/* <DimensionUI /> */}
-      <PokemonCard />
+      <PokemonCard pokemons={pokemonData} />
     </View>
   );
 };
--- components/Pokemons/PokemonCard.js
+++ components/Pokemons/PokemonCard.js
@@ -1,11 +1,38 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, Text, ScrollView, Image} from 'react-native';
 
-export default function PokemonCard() {
+export default function PokemonCard({pokemons}) {
   return (
-    <View style={styles.card}>
-      <Text>Pokemon Cards</Text>
-    </View>
+    <ScrollView>
+      {pokemons?.map((item, index) => (
+        <View style={styles.card}>
+          <View style={styles.nameContainer}>
+            <Text style={{fontSize: 28, color: 'black'}}>{item.name}</Text>
+            <Text style={styles.hp}>🔥{item.hp}</Text>
+          </View>
+          <View>
+            <Image
+              source={item.image}
+              accessabilityLabel={` ${item.name} pokemon`}
+              style={styles.image}
+              resizeMode="contain"
+            />
+            <Text style={{color: 'black', fontWeight: 'bold'}}>
+              TYPE:{' '}
+              <Text style={{fontSize: 20, color: 'black'}}>{item.type}</Text>
+            </Text>
+            <Text style={{fontWeight: 'bold', color: 'black'}}>
+              <Text>MOVES:{'  '}</Text>
+              {item?.moves?.map(i => i + ',')}
+            </Text>
+            <Text style={{fontWeight: 'bold', color: 'black'}}>
+              <Text>WEAKNESS: {'  '}</Text>{' '}
+              <Text>{item?.weakness?.map(i => i + ',')}</Text>
+            </Text>
+          </View>
+        </View>
+      ))}
+    </ScrollView>
   );
 }
 
@@ -17,5 +44,22 @@ const styles = StyleSheet.create({
     padding: 16,
     margin: 16,
     elevation: 5,
+  },
+  highlightText: {
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  nameContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  hp: {
+    fontSize: 22,
+    color: 'black',
+  },
+  image: {
+    width: '100%',
+    height: 200,
+    marginBottom: 16,
   },
 });
