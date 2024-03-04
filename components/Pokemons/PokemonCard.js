996ee7050ac2065fc9ecc3040c7d1f29234bbb47
Adarsh Kumar Mishra <124551477+Adarshabinash@users.noreply.github.com>
2024-03-04 13:42:59 +0530

    Made changes to the styles for the card effect of the names and values

--- components/Pokemons/PokemonCard.js
+++ components/Pokemons/PokemonCard.js
@@ -3,8 +3,19 @@ import {View, StyleSheet, Text} from 'react-native';
 
 export default function PokemonCard() {
   return (
-    <View>
+    <View style={styles.card}>
       <Text>Pokemon Cards</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: 'white',
+    borderRadius: 16,
+    borderWidth: 2,
+    padding: 16,
+    margin: 16,
+    elevation: 5,
+  },
+});
