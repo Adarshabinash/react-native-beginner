6002efc21ae1cc07c0189722e5a34a446926656c
Adarsh Kumar Mishra <124551477+Adarshabinash@users.noreply.github.com>
2024-03-11 11:03:10 +0530

    Various props like autoComplete, autoCapitalize, secureTextEntry and others are discussed

--- pages/Formss/TextInputForm.js
+++ pages/Formss/TextInputForm.js
@@ -16,8 +16,12 @@ const TextInputForm = () => {
       <TextInput
         style={styles.input}
         value={name}
-        placeholder="Type here..."
+        placeholder="Type your name"
         onChangeText={setName} //?----Unlike we do in react, in onChangeText, we simply have to mention the state that will hold the updated state
+        secureTextEntry //! for the types password where we want to hide the inputs
+        // keyboardType="numeric"  //! for numeric or the ascii letters of the keyborad
+        autoCorrect={true} //?To enable autocorrect. We can keep it false to disable.
+        autoCapitalize="none" //?In case we don't wanna auto capitalize our texts. Default will enable it.
       />
       {name ? <Text style={styles.text}>My name is {name}</Text> : null}
     </SafeAreaView>
