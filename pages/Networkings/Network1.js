853d7008d63078d77c68538c1756eddc1be7ceab
Adarsh Kumar Mishra <124551477+Adarshabinash@users.noreply.github.com>
2024-03-21 11:05:15 +0530

    networking

--- pages/Networkings/Network1.js
+++ pages/Networkings/Network1.js
@@ -6,12 +6,17 @@ import {
   FlatList,
   ActivityIndicator,
   SafeAreaView,
+  TextInput,
+  Button,
 } from 'react-native';
 
 const Network1 = () => {
   const [postList, setPostList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [refreshing, setRefreshing] = useState(false);
+  const [postTitle, setPostTitle] = useState('');
+  const [postBody, setPostBody] = useState('');
+  const [isPosting, setIsPosting] = useState(false);
 
   const fetchData = async limit => {
     fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}`)
@@ -35,8 +40,48 @@ const Network1 = () => {
     fetchData(10);
   }, []);
 
+  const addPost = async () => {
+    setIsPosting(true);
+    fetch(`https://jsonplaceholder.typicode.com/posts`, {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        title: postTitle,
+        body: postBody,
+      }),
+    })
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setPostList([...data, ...postList]);
+        setIsPosting(false);
+      });
+  };
+
   return (
     <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Post Title"
+          value={postTitle}
+          onChangeText={setPostTitle}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Post Title"
+          value={postBody}
+          onChangeText={setPostBody}
+        />
+        <Button
+          title={isPosting ? 'Adding...' : 'Add Post'}
+          onPress={addPost}
+          disabled={isPosting}
+        />
+      </View>
       {isLoading ? (
         <SafeAreaView>
           <ActivityIndicator size="large" color="0000ff" />
@@ -96,5 +141,20 @@ const styles = StyleSheet.create({
   footerText: {
     fontSize: 18,
     alignSelf: 'center',
+  },
+  inputContainer: {
+    backgroundColor: 'white',
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+    margin: 16,
+  },
+  input: {
+    height: 40,
+    borderColor: 'grey',
+    borderWidth: 1,
+    marginBottom: 8,
+    padding: 8,
+    borderRadius: 8,
   },
 });
